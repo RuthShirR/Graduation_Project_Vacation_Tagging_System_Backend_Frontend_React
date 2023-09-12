37ff89e125e3383453bfb0ad87db8fbd96b5ec3b
RuthShirR <125210995+RuthShirR@users.noreply.github.com>
2023-09-12 10:48:58 +0300

    Add files via upload

---
+++ database/followers.json.sql
@@ -0,0 +1,30 @@
+/*
+-- Query: SELECT * FROM VacationsDB.Followers
+LIMIT 0, 1000
+
+-- Date: 2023-09-12 10:43
+*/
+INSERT INTO `` (`userId`,`vacationId`) VALUES (15,9);
+INSERT INTO `` (`userId`,`vacationId`) VALUES (16,9);
+INSERT INTO `` (`userId`,`vacationId`) VALUES (18,9);
+INSERT INTO `` (`userId`,`vacationId`) VALUES (15,10);
+INSERT INTO `` (`userId`,`vacationId`) VALUES (16,10);
+INSERT INTO `` (`userId`,`vacationId`) VALUES (15,11);
+INSERT INTO `` (`userId`,`vacationId`) VALUES (17,11);
+INSERT INTO `` (`userId`,`vacationId`) VALUES (15,12);
+INSERT INTO `` (`userId`,`vacationId`) VALUES (18,12);
+INSERT INTO `` (`userId`,`vacationId`) VALUES (20,12);
+INSERT INTO `` (`userId`,`vacationId`) VALUES (15,13);
+INSERT INTO `` (`userId`,`vacationId`) VALUES (18,13);
+INSERT INTO `` (`userId`,`vacationId`) VALUES (20,13);
+INSERT INTO `` (`userId`,`vacationId`) VALUES (15,14);
+INSERT INTO `` (`userId`,`vacationId`) VALUES (17,14);
+INSERT INTO `` (`userId`,`vacationId`) VALUES (15,15);
+INSERT INTO `` (`userId`,`vacationId`) VALUES (20,16);
+INSERT INTO `` (`userId`,`vacationId`) VALUES (15,17);
+INSERT INTO `` (`userId`,`vacationId`) VALUES (17,17);
+INSERT INTO `` (`userId`,`vacationId`) VALUES (18,17);
+INSERT INTO `` (`userId`,`vacationId`) VALUES (15,20);
+INSERT INTO `` (`userId`,`vacationId`) VALUES (18,20);
+INSERT INTO `` (`userId`,`vacationId`) VALUES (15,24);
+INSERT INTO `` (`userId`,`vacationId`) VALUES (15,40);
---
+++ database/users.sql
@@ -0,0 +1,17 @@
+/*
+-- Query: SELECT * FROM VacationsDB.Users
+LIMIT 0, 1000
+
+-- Date: 2023-09-12 10:44
+*/
+INSERT INTO `` (`id`,`uuid`,`firstName`,`lastName`,`email`,`password`,`isAdmin`) VALUES (15,'e66ffdac-78c6-493c-98d3-ea148acd0ee2','Rutty','Shiri','Ruthshir31@gmail.com','$2b$10$DHhLK5Hm3GMxauilp5rkkOQnNFuaq/0RHAw/FvJ4RpgyP37FVUajy',0);
+INSERT INTO `` (`id`,`uuid`,`firstName`,`lastName`,`email`,`password`,`isAdmin`) VALUES (16,'937433e2-4226-4552-a7ec-3aeec657fa47','Ruth','Siri','ruthshir30@gmail.com','$2b$10$.0AxSilQ7Kfxah6M8LMwH.81e2BSSkJ9aI1m4DLzdWxun40/tHyIy',0);
+INSERT INTO `` (`id`,`uuid`,`firstName`,`lastName`,`email`,`password`,`isAdmin`) VALUES (17,'85b3afe0-7e95-44e7-a7d4-274a627ebe4c','Chay','Lang','chaymonlang@gmail.com','$2b$10$nmgxzJmDB6ca2atlr8PRi.IWmWY5Y2JgiC0V4DiXcfzRMadEfbjxm',0);
+INSERT INTO `` (`id`,`uuid`,`firstName`,`lastName`,`email`,`password`,`isAdmin`) VALUES (18,'6e5d8e5a-c0cf-4778-8464-0cb287f87d63','chay','mon','chaymonlang111@gmail.com','$2b$10$KIVy46QPbVm3GCHyJjAsHONf4nVTZV8AyVmtiupggN5vdZ/dZbIEa',0);
+INSERT INTO `` (`id`,`uuid`,`firstName`,`lastName`,`email`,`password`,`isAdmin`) VALUES (19,'3204a982-ae4d-472f-8826-1ae697fe2cc3','rutty','Lang','ruttylang@gmail.com','$2b$10$Axqbm54wCJueuqXavu3uYenc5yx6l1GylqNUZyVf12qbb1JyBkMOa',0);
+INSERT INTO `` (`id`,`uuid`,`firstName`,`lastName`,`email`,`password`,`isAdmin`) VALUES (20,'45bf9568-d25c-4617-b41f-2994902eb3fc','noam','Rosenblum','noamlibi9@gmail.com','$2b$10$cxKEI4d8jtRzqNd/0jGgX..qnNDgVfEdK0N0WeGaMj4dycwAtxF2C',0);
+INSERT INTO `` (`id`,`uuid`,`firstName`,`lastName`,`email`,`password`,`isAdmin`) VALUES (21,'0950af98-5cf2-479d-8583-516dbab2d968','shir','ruthhh','ruthhh@gmail.com','$2b$10$.g50GJ4OaB7vQCQ1Q.wxr./ZRN1qSOVK73G5Sw79xPOtZWCMuEnfi',0);
+INSERT INTO `` (`id`,`uuid`,`firstName`,`lastName`,`email`,`password`,`isAdmin`) VALUES (46,'3adf6700-2820-4df8-9581-f89e2a96c9e1','ruth','shir','ruth.shir1212@example.com','$2a$10$LAGOFdqQ5xIgGI0hRvElsOeQ0HLG20jyuAeCiyDiXyxSwaAHOdBM2',0);
+INSERT INTO `` (`id`,`uuid`,`firstName`,`lastName`,`email`,`password`,`isAdmin`) VALUES (85,'42afcba1-536e-4b62-b290-0210fb66e770','Admin','Admin','Admin@gmail.com','$2a$10$Bjj1iUQoT7YdvfF5BdKIi.WCA2LYLQKYznxqbxBZBBGLP/7rSosYG',1);
+INSERT INTO `` (`id`,`uuid`,`firstName`,`lastName`,`email`,`password`,`isAdmin`) VALUES (86,'e423c0c5-e8be-4045-be24-b18d3cda969f','chay','mon','chaych@gmail.com','$2a$10$K/uldXWnH5HmPoGS1zx/8uL4VrKxGBcvRQ/Yvl4bb4DxzhyXdA7V6',0);
+INSERT INTO `` (`id`,`uuid`,`firstName`,`lastName`,`email`,`password`,`isAdmin`) VALUES (89,'00b5b181-3a0e-4794-9e6f-573a7b7b0576','check','checking','checking@gmail.com','$2a$10$SRQXGdAIMQKfS2tFoy.KeeIZQjoc.hEjSgeS..sqHtUY8ZK7u474O',0);
